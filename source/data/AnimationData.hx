b97ac6041d93232b4b8eaa99c9031f7d5ecad7de
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-05-09 00:03:52 -0500

    load animation data from a file

--- source/data/AnimationData.hx
+++ source/data/AnimationData.hx
@@ -1,10 +1,35 @@
 package data;
 
+import json2object.JsonParser;
+import lime.utils.Assets;
+
 class AnimationData
 {
+	public static final CURRENT_VERSION:Int = 0;
+
+	@:default(CURRENT_VERSION)
 	public var version:Int;
 
+	@:default([])
 	public var events:Array<Dynamic>;
 
 	public function new() {}
+
+	public function loadFromFile(filePath:String):AnimationData
+	{
+		if (!Assets.exists(filePath))
+		{
+			throw 'AnimationData / loadFromFile : $filePath doesn\'t exist stupid!';
+		}
+
+		var data = new JsonParser<AnimationData>().fromJson(Assets.getText(filePath), filePath);
+
+		this.events = data.events;
+
+		switch (data.version) {}
+
+		this.version = CURRENT_VERSION;
+
+		return this;
+	}
 }
